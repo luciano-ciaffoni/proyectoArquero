047f6ea24e2cd331e33160bc42a8a13a074d11d4
luciano-ciaffoni <136344291+luciano-ciaffoni@users.noreply.github.com>
2023-11-30 22:40:49 -0300

    subir proyecto final

---
+++ java.js
@@ -0,0 +1,50 @@
+document.getElementById("descripcionFillol").addEventListener("click", function(){
+         let descripcionFillol   = document.getElementById("descripcion1")
+        if (descripcionFillol.style.display === "none") {
+                descripcionFillol.style.display = "block"
+        } else {
+            descripcionFillol.style.display = "none"
+            
+        }
+})
+
+document.getElementById("descripcionPumpido").addEventListener("click", function(){
+    let descripcionPumpido  = document.getElementById("descripcion2")
+   if (descripcionPumpido.style.display === "none") {
+           descripcionPumpido.style.display = "block"
+   } else {
+       descripcionPumpido.style.display = "none"
+       
+   }
+})
+
+document.getElementById("descripcionDibu").addEventListener("click", function(){
+    let descripcionDibu   = document.getElementById("descripcion3")
+   if (descripcionDibu.style.display === "none") {
+           descripcionDibu.style.display = "block"
+   } else {
+       descripcionDibu.style.display = "none"
+       
+   }
+})
+
+document.getElementById("descripcionCarrizo").addEventListener("click", function(){
+    let descripcionCarrizo   = document.getElementById("descripcion4")
+   if (descripcionCarrizo.style.display === "none") {
+           descripcionCarrizo.style.display = "block"
+   } else {
+       descripcionCarrizo.style.display = "none"
+       
+   }
+})
+
+
+document.getElementById("descripcionGatti").addEventListener("click", function(){
+    let descripcionGatti   = document.getElementById("descripcion5")
+   if (descripcionGatti.style.display === "none") {
+           descripcionGatti.style.display = "block"
+   } else {
+       descripcionGatti.style.display = "none"
+       
+   }
+})
